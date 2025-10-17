6b0c5347a78167baaf26ccca0d9b7830514fc931
Hirata-Matheus <matheushirata2001@outlook.com>
2025-10-17 15:44:30 -0300

    Adicionando github pages 2

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     nightwatchPlugin(),
   ],
-  base: '/meu-amor/',
+  base: process.env.NODE_ENV === 'production' ? '/great-memories/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
